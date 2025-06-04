491bd6ff89adab3cbf087edb3e87a5b6171afd03
Raanan Avidor <ravidor@pendo.io>
2025-06-04 16:54:36 +0300

    Update app version to 1.1.2 - Incremented versionCode to 4 and versionName to "1.1.2" in build.gradle.kts for the next release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.ravidor.forksure"
         minSdk = 29
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
