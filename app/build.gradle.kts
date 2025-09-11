8ca63d507a995b4164a0fd53b213f15f0514823d
Raanan Avidor <ravidor@pendo.io>
2025-09-11 09:12:38 +0300

    Release 1.3.0: bump versionCode to 13 and versionName to 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.ravidor.forksure"
         minSdk = 29
         targetSdk = 36
-        versionCode = 12
-        versionName = "1.3.0.rc3"
+        versionCode = 13
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "com.ravidor.forksure.HiltTestRunner"
         
