9df9b0a07d101666d4ef7ba77f102a9d8c91ddb6
Raanan Avidor <ravidor@pendo.io>
2025-09-24 20:45:04 +0300

    chore: update tests and build config\n\n- Update EnhancedErrorHandlerTest, MessageDisplayHelperTest, RecipeSharingHelperTest\n- Add ErrorHandlerTest\n- Tweak app/build.gradle.kts (#39)

--- app/src/test/java/com/ravidor/forksure/MessageDisplayHelperTest.kt
+++ app/src/test/java/com/ravidor/forksure/MessageDisplayHelperTest.kt
@@ -1,12 +1,20 @@
 package com.ravidor.forksure
 
+import com.google.common.truth.Truth.assertThat
 import org.junit.Test
-import org.junit.Assert.*
 
 class MessageDisplayHelperTest {
+
     @Test
-    fun sampleTest() {
-        // TODO: Implement test
-        assertTrue(true)
+    fun userMessage_defaultDurations_matchByType() {
+        val mSuccess = UserMessage("ok", MessageType.SUCCESS)
+        val mError = UserMessage("bad", MessageType.ERROR)
+        val mInfo = UserMessage("hi", MessageType.INFO)
+        val mWarn = UserMessage("warn", MessageType.WARNING)
+
+        assertThat(mSuccess.duration.name).isEqualTo("Short")
+        assertThat(mError.duration.name).isEqualTo("Long")
+        assertThat(mInfo.duration.name).isEqualTo("Short")
+        assertThat(mWarn.duration.name).isEqualTo("Long")
     }
-} 
+}
